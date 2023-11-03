115479a627cc3fc1826c5ac80ff37f2ac33d994e
Quentin Preik <quentin@preik.net>
2023-11-03 06:42:45 -0400

    Add project files.

---
+++ storagecontroller/StorageControllerModSystem.cs
@@ -0,0 +1,21 @@
+﻿using Vintagestory.API.Common;
+using Vintagestory.API.Server;
+using Vintagestory.API.Client;
+
+
+namespace StorageController
+{
+    
+    public class StorageControllerModSystem : ModSystem
+    {
+        ICoreClientAPI capi;
+        ICoreServerAPI sapi;
+
+        public override void StartPre(ICoreAPI api)
+        {
+            base.StartPre(api);
+            if (api is ICoreClientAPI) { capi=api as ICoreClientAPI; }
+            else if (api is ICoreServerAPI) { sapi=api as ICoreServerAPI; }
+        }
+    }
+}
