fbd696f03d25fabf66e57f0f688492d5e4e73ce9
Quentin Preik <quentin@preik.net>
2023-11-06 15:50:07 -0500

    Begin coding scm

--- storagecontroller/ItemStorageLinker.cs
+++ storagecontroller/ItemStorageLinker.cs
@@ -21,12 +21,16 @@ namespace storagecontroller
         public override void OnHeldInteractStart(ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel, EntitySelection entitySel, bool firstEvent, ref EnumHandHandling handling)
         {
             ICoreAPI api = byEntity.Api;
-            
+            if (api == null || slot == null || slot.Itemstack == null||slot.Itemstack.Item==null)
+            {
+                base.OnHeldInteractStart(slot, byEntity, blockSel, entitySel, firstEvent, ref handling);
+                return;
+            }
             // get targetted block
             BlockEntity targetentity = api.World.BlockAccessor.GetBlockEntity(blockSel.Position);
             
             //if the block is a storage controller then set it as the target
-            if (api!=null && slot!=null && slot.Itemstack!=null && slot.Itemstack.Item!=null&& targetentity != null && targetentity is StorageControllerMaster) {
+            if (targetentity is StorageControllerMaster) {
                 slot.Itemstack.Attributes.SetBlockPos(islkey, blockSel.Position);
                 slot.Itemstack.Attributes.SetString(isldesc, blockSel.Position.ToLocalPosition(api).ToString());
                 if (api is ICoreServerAPI)
@@ -37,7 +41,18 @@ namespace storagecontroller
                 return;
             }
 
-            base.OnHeldInteractStart(slot, byEntity, blockSel, entitySel, firstEvent, ref handling);
+            //otherwise we will see if it's a valid storage device and tell controller about it
+            
+            //quit if islkey is not set
+            if (!slot.Itemstack.Attributes.HasAttribute(islkey)) { base.OnHeldInteractStart(slot, byEntity, blockSel, entitySel, firstEvent, ref handling); return; }
+
+            //Check for valid SCM
+            BlockPos scmpos = slot.Itemstack.Attributes.GetBlockPos(islkey);
+            if (scmpos == null) { base.OnHeldInteractStart(slot, byEntity, blockSel, entitySel, firstEvent, ref handling); return; }
+            StorageControllerMaster scm = api.World.BlockAccessor.GetBlockEntity(scmpos) as StorageControllerMaster;
+            if (scm == null) { base.OnHeldInteractStart(slot, byEntity, blockSel, entitySel, firstEvent, ref handling); return; }
+            if (api is ICoreServerAPI) { scm.AddContainer(slot,byEntity,blockSel); }
+            handling=EnumHandHandling.Handled;
 
         }
         public override string GetHeldItemName(ItemStack itemStack)
@@ -45,7 +60,7 @@ namespace storagecontroller
             if (itemStack != null && itemStack.Attributes.HasAttribute(isldesc))
             {
                 
-                return "Linking to "+itemStack.Attributes.GetString(isldesc);
+                return "Storage Linker (Linking to "+itemStack.Attributes.GetString(isldesc)+")";
             }
             return base.GetHeldItemName(itemStack);
         }
--- storagecontroller/StorageControllerMaster.cs
+++ storagecontroller/StorageControllerMaster.cs
@@ -7,28 +7,93 @@ using Vintagestory.API.Common;
 using Vintagestory.API.Server;
 using Vintagestory.API.Client;
 using Vintagestory.GameContent;
-
+using Vintagestory.API.MathTools;
+using Vintagestory.API.Datastructures;
+using ProtoBuf;
+using Newtonsoft.Json;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace storagecontroller
 {
     public class StorageControllerMaster:BlockEntityGenericTypedContainer
     {
+        public static string containerlistkey = "containerlist";
+        List<BlockPos> containerlist; //TODO: add to treeattributes
         public override void Initialize(ICoreAPI api)
         {
             base.Initialize(api);
             if (Api is ICoreServerAPI) { RegisterGameTickListener(OnServerTick, 100); }
         }
 
+        //stuff to do every so often
         public void OnServerTick(float dt)
         {
+            //Check if we have any inventory to bother with
+            if (this.Inventory == null || this.Inventory.Empty) { return; }
+            
             //Manage linked container list
             // - only check so many blocks per tick
-
+            List<BlockPos> prunelist = new List<BlockPos>(); //This is a list of invalid blockpos that should be deleted from list
+            List<BlockEntityGenericTypedContainer> validcontainers = new List<BlockEntityGenericTypedContainer>(); //list of usable containers
+            if (containerlist != null) {
+                
+                foreach (BlockPos pos in containerlist)
+                {
+                    BlockEntityGenericTypedContainer thiscont = Api.World.BlockAccessor.GetBlockEntity(pos) as BlockEntityGenericTypedContainer;
+                    //no valid container here so set to remove this location ***Should we do that or just let new containers be placed later??
+                    if (thiscont == null)
+                    {
+                        prunelist.Add(pos);
+                    }
+                    else
+                    {
+                        validcontainers.Add(thiscont);
+                    }
+                }
+            }
             //Need to populate containers if this container has inventory
             // - from list of container first find a stack or locked container with inventory
             // - then push in inventory if possible
             // - otherwise look for empty slots we can put our inventory into
 
+        }
+
+        //add a container to the list of managed containers (usually called by a storage linker)
+        public void AddContainer(ItemSlot slot, EntityAgent byEntity, BlockSelection blockSel)
+        {
+            //don't want to link to ourself!
+            if (blockSel.Position == this.Pos) { return; } 
+            //check for valid container
+            BlockEntityGenericTypedContainer cont = Api.World.BlockAccessor.GetBlockEntity(blockSel.Position) as BlockEntityGenericTypedContainer;
+            if (cont == null) { return; }
+            //ensure block isn't reinforced
+
+            //ensure player as access rights
+            
+            //if container isn't on list then add it
+            if (containerlist==null) { containerlist = new List<BlockPos>();}
+            if (!containerlist.Contains(blockSel.Position)) {
+                if (Api is ICoreServerAPI)
+                {
+                    containerlist.Add(blockSel.Position); MarkDirty();
+                }
+            }
+
+        }
+        public override void FromTreeAttributes(ITreeAttribute tree, IWorldAccessor worldForResolving)
+        {
+            base.FromTreeAttributes(tree, worldForResolving);
+            var asString = tree.GetString(containerlistkey);
+            if (asString!=null)
+            {
+                containerlist=JsonConvert.DeserializeObject<List<BlockPos>>(asString);
+            }
+        }
+        public override void ToTreeAttributes(ITreeAttribute tree)
+        {
+            var asString = JsonConvert.SerializeObject(containerlist);
+            tree.SetString(containerlistkey, asString);
+            base.ToTreeAttributes(tree);
         }
     }
 }
