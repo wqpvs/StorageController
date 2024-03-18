39a10cc7bab19dffdb2bfcb02a0531931cb26eb4
MisterAndyDandy <andy-18-@hotmail.com>
2024-03-18 02:31:32 +0000

    Updated
    New Shape for storagecontrollerupgrade

--- storagecontroller/BlockStorageController.cs
+++ storagecontroller/BlockStorageController.cs
@@ -6,11 +6,13 @@ using System.Text.Json.Nodes;
 using System.Text.Json.Serialization;
 using Vintagestory.API.Client;
 using Vintagestory.API.Common;
+using Vintagestory.API.Config;
 using Vintagestory.API.Datastructures;
 using Vintagestory.API.MathTools;
 using Vintagestory.API.Server;
 using Vintagestory.API.Util;
 using Vintagestory.GameContent;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace storagecontroller
 {
@@ -81,6 +83,12 @@ namespace storagecontroller
 
         }
 
+        private float meshangle = 0f;
+
+        private string type = string.Empty;
+
+        private List<BlockPos> Pos = new List<BlockPos>();
+
         private bool TryApplyUpgrade(IWorldAccessor world, IPlayer byPlayer, StorageControllerMaster storageControllerMaster)
         {
             if (byPlayer?.InventoryManager?.ActiveHotbarSlot == null)
@@ -112,28 +120,43 @@ namespace storagecontroller
                 return false; // No matching metal type found
 
             // Construct the name for the upgraded block
-            string upgradeBlockName = "storagecontroller:" + "storagecontroller" + metaltype + "-east";
+            string upgradeBlockName = "storagecontroller:" + "storagecontroller" + metaltype + "-" + Code.EndVariant();
 
             // Make sure it's not the same block
             if (upgradeBlockName.Equals(Code.ToString()))
                 return false; // Storage controller already at the highest tier
 
             // Check if the block exists
+            meshangle = storageControllerMaster.MeshAngle;
+            type = storageControllerMaster.type;
             BlockGenericTypedContainer block = world.BlockAccessor.GetBlock(new AssetLocation(upgradeBlockName)) as BlockGenericTypedContainer;
             if (block == null)
                 return false; // Block not found
 
-            // Set the block and mark it as dirty
-            world.BlockAccessor.SetBlock(block.Id, storageControllerMaster.Pos);
-            storageControllerMaster.MarkDirty();
+            // Make sure if there is anything in Inventory it will get drop before upgrading
+            storageControllerMaster.Inventory.DropAll(byPlayer.Entity.Pos.AsBlockPos.ToVec3d());
 
-            // Consume one item from the player's hand
+            // Let see if we got any linked Containers
+            Pos = storageControllerMaster.ContainerList;
+
+            // Set the block
+            api.World.BlockAccessor.SetBlock(block.Id, byPlayer.CurrentBlockSelection.Position);
+            StorageControllerMaster storageController = api.World.BlockAccessor.GetBlockEntity(byPlayer.CurrentBlockSelection.Position) as StorageControllerMaster;
+
+            //Make sure angle is the same as before and same for type.
+            storageController.MeshAngle = meshangle;
+            storageController.type = type;
+
+
+            // Let add linked Container back to the new block
+            storageController.SetContainers(Pos);
 
             if (api is ICoreClientAPI capi)
             {
                 capi.World.PlaySoundAt(new AssetLocation("game:sounds/tool/reinforce"), byPlayer, capi.World.Player);
             }
 
+            // Consume one item from the player's hand
             byPlayer.InventoryManager.ActiveHotbarSlot.TakeOut(1);
             byPlayer.InventoryManager.ActiveHotbarSlot.MarkDirty();
             byPlayer.InventoryManager.BroadcastHotbarSlot();
